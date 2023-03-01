9cb540f2290f968b351b949102315601b8970a61
Fernando Cordero <fernan942@gmail.com>
2023-03-01 09:33:48 -0300

    ADD: final code, readme

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,20 +1,68 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-function App () {
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
+  // movimiento del cursor
   useEffect(() => {
     console.log('effect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      // console.log({ clientX, clientY }, 'HandleMove')
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    // limpiar el efecto.
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias, antes de ejecutar el efecto de nuevo
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  // cambiar nombre de la clase del body
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
   }, [enabled])
 
   return (
     <>
-      <h3>Proyecto numero 3</h3>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} Seguir puntero
       </button>
     </>
+  )
+}
+function App () {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
--- projects/README.md
+++ projects/README.md
@@ -7,3 +7,20 @@ $ npm create vite@latest
 $ cd project-name
 
 $ npm install
+
+en la consola del inspector corremos getEventListeners para ver las suscripciones a los eventos
+
+### Dependencias del useEfect
+
+useEfect:
+
+- [] -> solo se ejecuta una vez cuando se monta el componente
+- [state_name] -> se ejecuta cuando cambia el estado y cuando se monta el componente
+- undefined -> se ejecuta cada vez que se renderiza el componente
+
+Despliegue:
+Se puede desplegar solo arrastrando el dist hacia la ruta.
+
+$ npm run build
+
+https://app.netlify.com/drop/

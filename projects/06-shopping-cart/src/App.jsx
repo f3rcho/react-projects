255694f998ca89060c01a72b7224e9add517412d
Fernando Cordero <fernan942@gmail.com>
2023-04-15 20:20:29 -0400

    seudo cart

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -4,6 +4,7 @@ import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { IS_DEVELOPMENT } from './config'
 import { useFilters } from './hooks/useFilters'
+import { Cart } from './components/Cart'
 
 function App () {
   const { filterProducts } = useFilters()
@@ -12,6 +13,7 @@ function App () {
   return (
     <>
       <Header />
+      <Cart />
       <Products products={filteredProducts} />
       {IS_DEVELOPMENT && <Footer />}
     </>

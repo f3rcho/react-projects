fdcdb770e547c86d7183093de444bd968c5e35a9
Fernando Cordero <fernan942@gmail.com>
2023-05-05 10:57:46 -0400

    Fetching data

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,14 +1,31 @@
-import withResults from '../mocks/with-results.json'
+import { useState } from 'react'
+import noResults from '../mocks/no-results.json'
 
-export function useMovies () {
-  const movies = [withResults]
-  const mappedMovies = movies.map((movie) => ({
+export function useMovies ({ search }) {
+  const [responseMovies, setResponseMovies] = useState([])
+
+  const movies = responseMovies.Search
+  const mappedMovies = movies?.map((movie) => ({
     id: movie.imdbID,
     title: movie.Title,
     year: movie.Year,
     poster: movie.Poster
   }))
+
+  const getMovies = () => {
+    if (search) {
+      fetch(`http://www.omdbapi.com/?apikey=c17541ea&s=${search}`)
+        .then(res => res.json())
+        .then(json => {
+          setResponseMovies(json)
+        })
+    } else {
+      setResponseMovies(noResults)
+    }
+  }
+
   return {
-    movies: mappedMovies
+    movies: mappedMovies,
+    getMovies
   }
 }

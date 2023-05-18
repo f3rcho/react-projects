4b82a88da1300ba9c5c7a6569e702539e230e8fe
Fernando Cordero <fernan942@gmail.com>
2023-05-18 15:41:31 -0400

    ADD: MPA

--- projects/07-f3rcho-router/src/App.tsx
+++ projects/07-f3rcho-router/src/App.tsx
@@ -1,7 +1,37 @@
+import { useState } from 'react'
 import './App.css'
 
+function HomePage () {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Welcome to my page</p>
+      <a href='/about'>Ir a About Page</a>
+    </>
+  )
+}
+
+function AboutPage () {
+  return (
+    <>
+      <h1>About</h1>
+      <div>
+        <img src='https://pbs.twimg.com/profile_images/1546490562303369216/tjKj1P01_400x400.jpg' alt='twitter profile' />
+        <p>Hello, I'm Fernando and I'm creating a clon fo react router</p>
+      </div>
+      <a href='/'>Go to Home Page</a>
+    </>
+  )
+}
+
 function App () {
-  return <h1>f3rcho router</h1>
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+  return (
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
+  )
 }
 
 export default App

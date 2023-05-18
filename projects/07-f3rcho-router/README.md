8541c688d8b09dea421c76603b68c3360e52566f
Fernando Cordero <fernan942@gmail.com>
2023-05-18 15:56:42 -0400

    ADD: SPA

--- projects/07-f3rcho-router/README.md
+++ projects/07-f3rcho-router/README.md
@@ -2,3 +2,7 @@
 simple.css
 add this to index.html
 <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+
+## Tips
+- there not native way to listen the pushstate event. To navigate forward.
+- One hint to know if It is an SPA is that the favicon does not reload when changing between pages
--- projects/07-f3rcho-router/src/App.tsx
+++ projects/07-f3rcho-router/src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate (href) {
+  window.history.pushState({}, '', href)
+  // create event when url changed
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  // whichever wants to listen the event
+  window.dispatchEvent(navigationEvent)
+}
 
 function HomePage () {
   return (
     <>
       <h1>Home</h1>
       <p>Welcome to my page</p>
-      <a href='/about'>Ir a About Page</a>
+      {/* for intance to simplify we use the button  */}
+      <button onClick={() => navigate('/about')}>Ir a About Page</button>
     </>
   )
 }
@@ -17,15 +28,28 @@ function AboutPage () {
       <h1>About</h1>
       <div>
         <img src='https://pbs.twimg.com/profile_images/1546490562303369216/tjKj1P01_400x400.jpg' alt='twitter profile' />
-        <p>Hello, I'm Fernando and I'm creating a clon fo react router</p>
+        <p>Hello, I'm Fernando and I'm creating a clon of react router</p>
       </div>
-      <a href='/'>Go to Home Page</a>
+      {/* for intance to simplify we use the button  */}
+      <button onClick={() => navigate('/')}>Go to Home Page</button>
     </>
   )
 }
 
 function App () {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  // suscription to event
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, [])
   return (
     <main>
       {currentPath === '/' && <HomePage />}

609f98ac57a721489eb5db1069c1daed4ec8d1e6
Fernando Cordero <fernan942@gmail.com>
2023-05-22 06:07:16 -0400

    ADD: keyboad and right click button functionality

--- projects/07-f3rcho-router/src/components/Link.jsx
+++ projects/07-f3rcho-router/src/components/Link.jsx
@@ -8,8 +8,14 @@ export function navigate (href) {
 
 export function Link ({ target, to, ...props }) {
   const handleClick = (event) => {
-    event.preventDefault()
-    navigate(to)
+    const isMainEvent = event.button === 0 // primary click
+    const isModifiedEvent = event.metaKey || event.altKey || event.ctrlKey || event.shiftKey
+    const isManageableEvent = target === undefined || target === '_self'
+
+    if (isMainEvent && isManageableEvent && !isModifiedEvent) {
+      event.preventDefault()
+      navigate(to)
+    }
   }
 
   return <a onClick={handleClick} href={to} target={target} {...props} />
